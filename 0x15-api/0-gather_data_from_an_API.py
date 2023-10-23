196843011ecfde53d4be8ec3729d92f7f23b9762
kerzers <ahra.ahlame@gmail.com>
2023-10-23 11:27:50 -0700

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Python script that use a given REST API
+for a given employee ID, returns information about his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    count = 0
+    task_count = 0
+    u_id = eval(sys.argv[1])
+    t_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(u_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+    todos = requests.get(t_url).text
+    user = requests.get(user_url).text
+
+    todo_list = json.loads(todos)
+    user_dict = json.loads(user)
+
+    name = user_dict["name"]
+
+    for elemt in todo_list:
+        for k, v in elemt.items():
+            if k == "completed" and v is True:
+                task_count += task_count
+        count += 1
+    print(f"Employee {name} is done with tasks({task_count}/{count}):")
+
+    for elemt in todo_list:
+        for k, v in elemt.items():
+            if k == "completed" and v is True:
+                print(f"\t {elemt['title']}")
