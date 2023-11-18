e9ab2e15b03c70631bfd7dbf194971b0cdc385ee
kerzers <ahra.ahlame@gmail.com>
2023-11-18 09:25:33 -0800

    fix limit user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# fix the user files limit
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i 's/nofile 5/nofile unlimited/; s/nofile 4/nofile unlimited/' /etc/security/limits.conf'
+}
